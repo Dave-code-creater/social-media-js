9f8554c83fdb2078de8d398affeb0978eaae54e9
Dave-code-creater <dat981dat@gmail.com>
2025-11-06 09:41:49 -0500

    Connect to Mongodb
    Signed-off-by: Dave-code-creater <dat981dat@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
+const connectDB = require("./config/db")
 const app = express();
-
+// Connect DB
+connectDB()
 app.get("/", (req, res) => res.send("API running"));
-
 
 const PORT = process.env.PORT || 3000;
 
