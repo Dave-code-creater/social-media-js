b752bc47df2e92ad992cf61f59da5d5e81875724
Dave-code-creater <dat981dat@gmail.com>
2025-11-06 09:50:38 -0500

    Add routes and api endpoint
    Signed-off-by: Dave-code-creater <dat981dat@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const app = express();
 // Connect DB
 connectDB()
 app.get("/", (req, res) => res.send("API running"));
-
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 const PORT = process.env.PORT || 3000;
 
 
