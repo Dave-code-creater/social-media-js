287bb85517be2f6ddff90ef41ed3a719260b5e9f
Dave-code-creater <dat981dat@gmail.com>
2025-11-12 15:39:07 -0500

    Implement user registration with validation, password hashing, and gravatar support; add profile model schema

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,15 +1,77 @@
 const express = require("express")
 const router = express.Router()
+const { check, validationResult } = require("express-validator")
 const User = require("../../models/User")
-const auth = require("../../middleware/auth")
+const gravatar = require("gravatar")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const config = require("config")
 
 // @route:   GET api/users
 // @desc:    test route
 // @access:  public
-router.get("/", auth, async (req, res) => {
+router.post("/", [
+    check("name", 'Name is required').not().isEmpty(),
+    check("email", 'Please include a valid email').isEmail(),
+    check("password", "Please enter a password with 6 or more characters").isLength({min: 6})
+], async (req,res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    
+    const {name, email, password } = req.body
+
     try {
-        const user = await User.findById(req.user.id).select("-password");
-        res.json(user)
+        // See if user exists
+        let user = await User.findOne({email: email})
+
+        if (user){
+            res.status(400).json({error: [{msg: "User already exist"}]})
+        }
+
+        // Get users gravatar
+        const avatar = gravatar.url(email, {
+            size: '200',
+            rating: 'pg',
+            default: 'mm'
+        })
+
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+
+        const salt = await bcrypt.genSalt(10);
+
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Encrypt password
+
+        // Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id
+
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get("jwtSecret"), 
+            {expiresIn: 360000},
+            (err, token) => {
+                if (err) throw err;
+                res.json({token});
+            }
+        );
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error')
